252caeb281d1fdba8e9c83e6f59080b78196026c
Wes Gilleland <wgillela@opentext.com>
2019-09-27 12:27:31 -0400

    Update

--- ProfileUtils.psm1
+++ ProfileUtils.psm1
@@ -7,73 +7,113 @@ function Test-Command {
     [OutputType([Boolean])]
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [String]$Command
     )
     return $null -ne (Get-Command $Command -ErrorAction SilentlyContinue)
 }
-if (!(Get-Command Set-EnvironmentVariable)) {
+function Set-EnvironmentVariable {
     <#
-        .Synopsis
-        Set an environment variable in the current session
-        .Description
-        Set an environment variable in the current session with the option to persist between sessions
-        .Parameter Name
-        Specifies the name of the variable to set. If it does not exist and the `-Force` parameter is not used, this will throw an error
+    .SYNOPSIS
+    Creates or sets an environment variable.
+
+    .DESCRIPTION
+    Uses the .NET [Environment class](http://msdn.microsoft.com/en-us/library/z8te35sa) to create or set an environment variable in the Process, User, or Machine scopes.
+
+    Changes to environment variables in the User and Machine scope are not picked up by running processes.  Any running processes that use this environment variable should be restarted.
+
+    .LINK
+    http://msdn.microsoft.com/en-us/library/z8te35sa
+
+    .EXAMPLE
+    Set-EnvironmentVariable -Name 'MyEnvironmentVariable' -Value 'Value1' -ForProcess
+
+    Creates the `MyEnvironmentVariable` with an initial value of `Value1` in the process scope, i.e. the variable is only accessible in the current process.
+
+    .EXAMPLE
+    Set-EnvironmentVariable -Name 'MyEnvironmentVariable' -Value 'Value1' -ForComputer
+
+    Creates the `MyEnvironmentVariable` with an initial value of `Value1` in the machine scope, i.e. the variable is accessible in all newly launched processes.
     #>
-    function Set-EnvironmentVariable {
-        [CmdletBinding()]
-        Param(
-            [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
-            [ValidateScript( { Test-Path ([Path]::GetFullPath($_)) })]
-            [String]$Value,
-            [Parameter()]
-            [switch]$Force,
-            [Parameter()]
-            [switch]$Persist
-        )
-        DynamicParam {
-            $ParamDictionary = [RuntimeDefinedParameterDictionary]::new()
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        # The name of environment variable to add/set.
+        [Parameter(Mandatory = $true)]
+        [string]$Name,
+        # The environment variable's value.
+        [Parameter(Mandatory = $true)]
+        [string]$Value,
+        # Sets the environment variable for the current computer.
+        [switch]$ForComputer,
+        # Sets the environment variable for the current user.
+        [switch]$ForUser,
+        # Sets the environment variable for the current process.
+        [switch]$ForProcess,
+        # Set the variable in the current PowerShell session's `env:` drive, too. Normally, you have to restart your session to see the variable in the `env:` drive.
+        [switch]$Force
+    )
+    if ( -not $ForProcess -and -not $ForUser -and -not $ForComputer ) {
+        Write-Error -Message "Environment variable target not specified. You must supply one of the ForComputer, ForUser, or ForProcess switches."
+        return
+    }
+    Invoke-Command -ScriptBlock {
+        if ( $ForComputer ) { [EnvironmentVariableTarget]::Machine }
+        if ( $ForUser ) { [EnvironmentVariableTarget]::User }
+        if ( $ForProcess ) { [EnvironmentVariableTarget]::Process }
+    } |
+        Where-Object { $PSCmdlet.ShouldProcess( "$_-level environment variable '$Name'", "set") } |
+        ForEach-Object { [Environment]::SetEnvironmentVariable( $Name, $Value, $_ ) }
+}
 
-            $Target = [RuntimeDefinedParameter]::new('Target', [string], @(
-                    [ParameterAttribute]@{
-                        HelpMessage                     = 'Whose path to modify'
-                        Mandatory                       = $false
-                        ValueFromPipelineByPropertyName = $false
-                    }
-                    [ValidateSetAttribute][string[]]([EnvironmentVariableTarget].GetEnumNames())
-                ))
-            $Target.Value = "User"
-            $ParamDictionary.Add('Target', $Target)
+function Remove-EnvironmentVariable {
+    <#
+    .SYNOPSIS
+    Removes an environment variable.
 
-            $ParamDictionary.Add('Name', [RuntimeDefinedParameter]::new('Name', [string], @(
-                        [ParameterAttribute]@{
-                            HelpMessage                     = 'Name of Environment Variable'
-                            Mandatory                       = $True
-                            ValueFromPipelineByPropertyName = $false
-                        }
-                        [ValidateSetAttribute][string[]](Get-ChildItem -Path Env:\ | Select-Object -ExpandProperty "Path")
-                        [ValidateScriptAttribute]::new( {
-                                if ((-not $Force) -and (-not (Test-Path "Env:\`"$_`""))) {
-                                    throw "No environment variable named ($_) found. Use -Force to create it"
-                                }
-                                return $true
-                            })
-                    )))
+    .DESCRIPTION
+    Uses the .NET [Environment class](http://msdn.microsoft.com/en-us/library/z8te35sa) to remove an environment variable from the Process, User, or Computer scopes.
 
-            return $ParamDictionary
-        }
-        Begin {
-            $Target = "$($Target.Value)"
-            [EnvironmentVariableTarget]$ResolvedTarget = [EnvironmentVariableTarget]::$Target
-        }
-        Process {
-            if ($Persist) {
-                [Environment]::SetEnvironmentVariable($Name, $Value, $ResolvedTarget)
-            }
-            else {
-                New-Item -Path "Env:\`"$($Name.Value)`"" -Value $Value -Force
-            }
+    Changes to environment variables in the User and Machine scope are not picked up by running processes.  Any running processes that use this environment variable should be restarted.
+
+    Normally, you have to restart your PowerShell session/process to no longer see the variable in the `env:` drive. Use the `-Force` switch to also remove the variable from the `env:` drive. This functionality was added in Carbon 2.3.0.
+
+    .LINK
+    http://msdn.microsoft.com/en-us/library/z8te35sa
+
+    .EXAMPLE
+    Remove-EnvironmentVariable -Name 'MyEnvironmentVariable' -ForProcess
+
+    Removes the `MyEnvironmentVariable` from the process scope.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        # The environment variable to remove.
+        [Parameter(Mandatory = $true)]
+        [string]$Name,
+        # Removes the environment variable for the current computer.
+        [switch]$ForComputer,
+        # Removes the environment variable for the current user.
+        [switch]$ForUser,
+        # Removes the environment variable for the current process.
+        [switch]$ForProcess,
+        # Remove the variable from the current PowerShell session's `env:` drive, too. Normally, you have to restart your session to no longer see the variable in the `env:` drive.
+        [switch]$Force
+    )
+    if ( -not $ForProcess -and -not $ForUser -and -not $ForComputer ) {
+        Write-Error -Message ('Environment variable target not specified. You must supply one of the ForComputer, ForUser, or ForProcess switches.')
+        return
+    }
+    Invoke-Command -ScriptBlock {
+        if ( $ForComputer ) { [EnvironmentVariableTarget]::Machine }
+        if ( $ForUser ) { [EnvironmentVariableTarget]::User }
+        if ( $ForProcess ) { [EnvironmentVariableTarget]::Process }
+    } |
+        Where-Object { $PSCmdlet.ShouldProcess( "$_-level environment variable '$Name'", "remove" ) } |
+        ForEach-Object {
+        $scope = $_
+        [Environment]::SetEnvironmentVariable( $Name, $null, $scope )
+        if ( $Force -and $scope -ne [EnvironmentVariableTarget]::Process ) {
+            [Environment]::SetEnvironmentVariable($Name, $null, 'Process')
         }
     }
 }
@@ -154,7 +194,7 @@ function Invoke-AtLocation {
     [CmdletBinding(SupportsShouldProcess)]
     Param(
         [Parameter(ValueFromPipeline, Mandatory = $true)]
-        [DirectoryInfo]$Location,
+        [IO.DirectoryInfo]$Location,
         [Parameter(Mandatory = $true)]
         [scriptblock]$ExecutionBlock
     )
@@ -181,17 +221,17 @@ function Invoke-LastCommand {
     )
     Process {
         $Commands = Get-History
-        if($n -gt $Commands.Count) {
+        if ($n -gt $Commands.Count) {
             $n = $n - $Commands.Count
         }
         $Command = Get-History -Count $n | Select-Object -first 1
-        if($Command.CommandLine -imatch "^$($PSCmdlet.MyInvocation.InvocationName)(\s+)?(\d+)?(\s+)?(.*)?(-whatif)?") {
+        if ($Command.CommandLine -imatch "^$($PSCmdlet.MyInvocation.InvocationName)(\s+)?(\d+)?(\s+)?(.*)?(-whatif)?") {
             Write-Debug "Clearing out command $Command"
             Clear-History -Count 1 -Id $Command.Id
             $Command = Get-History -Count $n | Select-Object -first 1
         }
         $id = $command.Id
-        if($PSCmdlet.ShouldProcess("Execute", "`"$($Command.CommandLine)`"")) {
+        if ($PSCmdlet.ShouldProcess("Execute", "`"$($Command.CommandLine)`"")) {
             Invoke-History -Id $id
         }
     }
@@ -227,7 +267,7 @@ function Compare-ObjectProperties {
     DynamicParam {
         $ParamDictionary = [RuntimeDefinedParameterDictionary]::new()
         $ValidProperties = (($ReferenceObject | Get-Member -MemberType Property, NoteProperty) + ($DifferenceObject | Get-Member -MemberType Property, NoteProperty)) | Select-Object -ExpandProperty Name | Sort-Object | Select-Object -Unique
-    
+
         $Include = [RuntimeDefinedParameter]::new('Include', [string[]], @(
                 [ParameterAttribute]@{
                     ParameterSetName = "IncludeProperties"
@@ -243,7 +283,7 @@ function Compare-ObjectProperties {
                 [ValidateSetAttribute][string[]]($ValidProperties)
             ))
         $ParamDictionary.Add($Exclude.Name, $Exclude)
-        
+
         return $ParamDictionary
     }
     Process {
@@ -279,7 +319,7 @@ function Compare-ObjectProperties {
 
 function Get-QAFolder {
     [CmdletBinding()]
-    [OutputType([DirectoryInfo])]
+    [OutputType([IO.DirectoryInfo])]
     Param(
         [string]$Name = "Wes"
     )
@@ -290,13 +330,13 @@ function Get-QAFolder {
 
 $Drives = Get-PSDrive
 $Drives |
-Where-Object { 
+    Where-Object {
     $_.Provider.Name -ieq "filesystem" `
-    -and $_.Name.Length -eq 1 `
-    -and $_.DisplayRoot -ne $null `
-    -and -not $_.DisplayRoot.StartsWith($_.Name) 
+        -and $_.Name.Length -eq 1 `
+        -and $_.DisplayRoot -ne $null `
+        -and -not $_.DisplayRoot.StartsWith($_.Name)
 } |
-ForEach-Object { 
+    ForEach-Object {
     $Pair = [DriveMapPair]::new($_.DisplayRoot, $_.Root)
     $DriveMaps[$Pair.Name] = $Pair
     New-PSDrive -Name $Pair.Name -PSProvider FileSystem -Root $Pair.UncPath | Out-Null
@@ -305,23 +345,11 @@ $ReleasesDirectory = $DriveMaps["Releases"].UncPath
 $NddResourcesDirectory = $DriveMaps["testcase-repo"].PrependUncPath("\Active\Resources")
 
 $Vms = @{
-    "win2016wes1.lab.opentext.com" = @{
-        ip = "10.21.86.25"
-        remoteSession = $null
-    }
-    "win2016wes2.lab.opentext.com" = @{
-        ip = "10.21.86.24"
-        remoteSession = $null
-    }
-    "win2016wes3.lab.opentext.com" = @{
-        ip = "10.21.86.45"
-        remoteSession = $null
-    }
     "lexmultidomains3.lab.opentext.com" = @{
-        ip = "10.21.86.162"
+        ip            = "10.21.86.162"
         remoteSession = $null
     }
- } | ForEach-Object { ([PSCustomObject]$_) }
+} | ForEach-Object { ([PSCustomObject]$_) }
 
 Export-ModuleMember -Function *-* `
     -Variable ReleasesDirectory, NddResourcesDirectory, DriveMaps, Vms `
